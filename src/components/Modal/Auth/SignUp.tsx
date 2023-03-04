cc3dd2f5c0b3f4a8da34fc3f9ed2ce50166ff3d6
Kinjalk <kinjalkbajpai@gmail.com>
2023-03-05 00:39:11 +0530

    Added error handling and google sign in

--- src/components/Modal/Auth/SignUp.tsx
+++ src/components/Modal/Auth/SignUp.tsx
@@ -2,14 +2,27 @@ import { authModalState } from '@/src/atoms/authModalAtom';
 import { Input, Button, Flex, Text } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { auth } from '../../../firebase/clientApp';
+import { FIREBASE_ERRORS } from '../../../firebase/errors';
 
 const SignUp: React.FC = () => {
 
-
     const setAuthModalState = useSetRecoilState(authModalState)
     const [signUpForm, setSignUpForm] = useState({ email: "", password: "", confirmPassword: "" })
+    const [error, setError] = useState('')
+    const [createUserWithEmailAndPassword, user, loading, userError] = useCreateUserWithEmailAndPassword(auth)
 
-    const onSubmit = () => { }
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+        event.preventDefault()
+        if (error) setError('')
+        if (signUpForm.password !== signUpForm.confirmPassword) {
+            setError("Passwords do not match");
+            return;
+        }
+        createUserWithEmailAndPassword(signUpForm.email, signUpForm.password)
+    }
 
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSignUpForm(prev => ({
@@ -20,10 +33,46 @@ const SignUp: React.FC = () => {
 
     return (
         <form onSubmit={onSubmit}>
-            <Input required name="email" placeholder="email" type="email" mb={2} onChange={onChange} fontSize="10pt" _placeholder={{ color: "gray.500" }} _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }} _focus={{ outline: "none", bg: "white", border: "1px solid", borderColor: "blue.500" }} bg="gray.50" />
-            <Input required name="password" placeholder="password" type="password" mb={2} onChange={onChange} fontSize="10pt" _placeholder={{ color: "gray.500" }} _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }} _focus={{ outline: "none", bg: "white", border: "1px solid", borderColor: "blue.500" }} bg="gray.50" />
-            <Input required name="confirmPassword" placeholder="confirm password" type="password" mb={2} onChange={onChange} fontSize="10pt" _placeholder={{ color: "gray.500" }} _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }} _focus={{ outline: "none", bg: "white", border: "1px solid", borderColor: "blue.500" }} bg="gray.50" />
-            <Button width="100%" height="36px" mt={2} mb={2} type="submit">Sign Up</Button>
+            <Input
+                required
+                name="email"
+                placeholder="email"
+                type="email"
+                mb={2}
+                onChange={onChange}
+                fontSize="10pt"
+                _placeholder={{ color: "gray.500" }}
+                _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
+                _focus={{ outline: "none", bg: "white", border: "1px solid", borderColor: "blue.500" }}
+                bg="gray.50" />
+            <Input
+                required
+                name="password"
+                placeholder="password"
+                type="password"
+                mb={2}
+                onChange={onChange}
+                fontSize="10pt"
+                _placeholder={{ color: "gray.500" }}
+                _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
+                _focus={{ outline: "none", bg: "white", border: "1px solid", borderColor: "blue.500" }}
+                bg="gray.50" />
+            <Input
+                required
+                name="confirmPassword"
+                placeholder="confirm password"
+                type="password"
+                mb={2}
+                onChange={onChange}
+                fontSize="10pt"
+                _placeholder={{ color: "gray.500" }}
+                _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
+                _focus={{ outline: "none", bg: "white", border: "1px solid", borderColor: "blue.500" }}
+                bg="gray.50" />
+            <Text textAlign="center" color="red" fontSize="10pt">
+                {error || FIREBASE_ERRORS[userError?.message as keyof typeof FIREBASE_ERRORS]}
+            </Text>
+            <Button width="100%" height="36px" mt={2} mb={2} type="submit" isLoading={loading}>Sign Up</Button>
             <Flex fontSize="9pt" justifyContent="center">
                 <Text mr={1}>Already a redditor?</Text>
                 <Text color="blue.500" fontWeight="700" cursor="pointer" onClick={() => setAuthModalState((prev) => ({
