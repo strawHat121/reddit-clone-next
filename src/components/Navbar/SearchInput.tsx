963d86de05c22c0654ac3276d11b3adaa0e753ad
Kinjalk <kinjalkbajpai@gmail.com>
2023-03-04 09:54:17 +0530

    Fixing children error

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -11,7 +11,9 @@ const SearchInput: React.FC<SearchInputProps> = () => {
     return (
         <Flex flexGrow={1} mr={2} align="center">
             <InputGroup>
-                <InputLeftElement pointerEvents="none" children={<SearchIcon color="gray.400" mb={1} />} />
+                <InputLeftElement pointerEvents="none">
+                    <SearchIcon color="gray.400" mb={1} />
+                </InputLeftElement>
                 <Input placeholder='Search Reddit' fontSize='10pt' _placeholder={{ color: "gray.500" }} _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }} _focus={{ outline: "none", border: "1px solid", borderColor: "blue.500" }} height="34px" bg="gray.50" />
             </InputGroup>
         </Flex>
