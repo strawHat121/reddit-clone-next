1a2a0dcb81d0bd01066921198316abd5110cebfa
Kinjalk <kinjalkbajpai@gmail.com>
2023-03-04 12:26:31 +0530

    Added login and signup modals

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,7 +1,9 @@
-import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from '@chakra-ui/react';
+import { Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text } from '@chakra-ui/react';
 import React, { Fragment } from 'react';
 import { useRecoilState } from 'recoil';
 import { authModalState } from '../../../atoms/authModalAtom';
+import AuthInputs from './AuthInputs';
+import OAuthButtons from './OAuthButtons';
 
 
 const AuthModal: React.FC = () => {
@@ -18,9 +20,20 @@ const AuthModal: React.FC = () => {
         <Modal isOpen={modalState.open} onClose={handleClose}>
             <ModalOverlay />
             <ModalContent>
-                <ModalHeader>Modal Title</ModalHeader>
+                <ModalHeader textAlign="center">
+                    {modalState.view === "login" && "Login"}
+                    {modalState.view === "signup" && "Sign Up"}
+                    {modalState.view === "resetPassword" && "Reset Password"}
+                </ModalHeader>
                 <ModalCloseButton />
-                <ModalBody>Here is the modal body</ModalBody>
+                <ModalBody display="flex" flexDirection="column" alignItems="center" justifyContent="center" pb={6}>
+                    <Flex direction="column" align="center" justify="center" width="70%">
+                        <OAuthButtons />
+                        <Text color="gray.500" fontWeight="700">OR</Text>
+                        <AuthInputs />
+                        {/* <ResetPassword /> */}
+                    </Flex>
+                </ModalBody>
             </ModalContent>
         </Modal>
     </Fragment>
